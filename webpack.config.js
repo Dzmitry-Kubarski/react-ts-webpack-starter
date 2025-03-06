27c9afc1011ca9e843455ade57f01b0308fa470e
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2025-03-06 11:40:03 +0300

    feat: added plugins and typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
     return {
         mode: env.mode ?? 'development',
-        entry: path.resolve(__dirname, 'src', 'index.js'),
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+
         output: {
             path: path.resolve(__dirname, 'dist'),
             filename: '[name]. [contenthash].js',
             clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, 'public', 'index.html'),
+            }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
         },
     };
 };
